7521055157bf8a1b3c65305a4f1e17588bb5cda3
ywferreira <yvonne.ferreira@gmail.com>
2022-11-07 20:22:37 -0500

    Update helloworld_finished.py

--- Ch2 - Basics/helloworld_finished.py
+++ Ch2 - Basics/helloworld_finished.py
@@ -10,6 +10,7 @@ def main():
     print("Nice to meet you again,", name)
     return name
 
+# first python program
 if __name__ == "__main__":
     newname = main()
     print(newname, " How are you")
