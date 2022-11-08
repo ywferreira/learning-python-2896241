2321f703109889ed2c4716b92dd30ecdc212955f
Yvonne Ferreira <yferreira@invidi.com>
2022-11-07 20:17:54 -0500

    Yvonne modified the file

--- Ch2 - Basics/helloworld_finished.py
+++ Ch2 - Basics/helloworld_finished.py
@@ -7,7 +7,9 @@
 def main():
     print("hello world!")
     name = input("What is your name? ")
-    print("Nice to meet you,", name)
+    print("Nice to meet you again,", name)
+    return name
 
 if __name__ == "__main__":
-    main()
+    newname = main()
+    print(newname, " How are you")
